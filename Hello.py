ccf30586dead0920b0a661a61eca37569adc3f52
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-05-14 10:18:50 +0000

    commit6_14.05.24

--- Hello.py
+++ Hello.py
@@ -2,31 +2,31 @@ import streamlit as st
 import requests
 
 st.header('An Overview of the Most Important Minerals')
+st.divider()
 st.markdown('This app can be used to get information about the most important minerals in geoscience. The information provided here is requested from mindat.org.')
-
+st.divider()
 selected_fields = st.multiselect(label='Which information do you want to get?', options=['name', 'mindat_formula', 'ima_formula', 'description_short'])
 fields_str = ",".join(selected_fields)
 
-# List of important minerals to display
-#important_minerals = garnets #["Olivine","Quarz","Calcite","Aragonite","Pyrope","Almandine", "Rhodolite","Spessartine","Grossular","Andradite","Uvarovite"]
+# Lists of important minerals
 
-garnets=["Pyrope","Almandine","Spessartine","Grossular"]
-alumnosilicates=["Kyanite","Sillimanite","Andalusite"]
-sulfates=["Gypsum","Baryte","Anhydryte"]
-Sulfides=["Pyrite","Chalkopyrite"]
-carbonates=["Calzite","Aragonite","Dolomite","Ankerite","Siderite","Magnesite"]
-feldspars=["Orthoclas","Albite","Sanidine","Microclin","Anorthite"]
-foids=["Nepheline","Leucite","Sodalite","Nosean","Hayun"]
-Pyroxenes=["Enstatite","Ferrosilite","Diopside","Hedenbergite","Jadeite","Augit","Omphazite"]
-clay_minerals=["Kaolinite","Illit","Montmorrilonite","Vermiculite"]
-micas=["Phlogopite","Annit","Eastonite","Muscovite","Phengit","Paragonite"]
-oxides=["Quartz","Rutile","Hematite","Illmenite","Chromite","Magnetite"]
-Amphiboles=["Tremolit","Aktinolit","Glaukophan","Riebeckit","Hornblende","Anthophyllit","Gredit"]
-layeredsilicates=["Lizadrdit","Chrysotile","Antigorite","Talk","Chlorite","Clinochlor","Chamosite"]
-ringsilicates=["Tourmaline"]
-groupsilicates=["Lawsonite","Epidote","Zoisite"]
-nesosilicates=["Olivine","Zircon","Titanite","Staurolite"]
-phosphates=["Apatite","Monazite"]
+garnets         = ["Pyrope", "Almandine", "Spessartine", "Grossular"]
+oxides          = ["Quartz", "Rutile", "Hematite", "Ilmenite", "Chromite", "Magnetite"]
+alumnosilicates = ["Kyanite","Sillimanite","Andalusite"]
+sulfates        = ["Gypsum","Baryte","Anhydryte"]
+Sulfides        = ["Pyrite","Chalkopyrite"]
+carbonates      = ["Calzite","Aragonite","Dolomite","Ankerite","Siderite","Magnesite"]
+feldspars       = ["Orthoclas","Albite","Sanidine","Microclin","Anorthite"]
+foids           = ["Nepheline","Leucite","Sodalite","Nosean","Hayun"]
+Pyroxenes       = ["Enstatite","Ferrosilite","Diopside","Hedenbergite","Jadeite","Augit","Omphazite"]
+clay_minerals   = ["Kaolinite","Illit","Montmorrilonite","Vermiculite"]
+micas           = ["Phlogopite","Annite","Eastonite","Muscovite","Phengite","Paragonite"]
+Amphiboles      = ["Tremolite","Aktinolite","Glaucophane","Riebeckite","Hornblende","Anthophyllit","Gredit"]
+layeredsilicates= ["Lizadrdit","Chrysotile","Antigorite","Talk","Chlorite","Clinochlor","Chamosite"]
+ringsilicates   = ["Tourmaline"]
+groupsilicates  = ["Lawsonite","Epidote","Zoisite"]
+nesosilicates   = ["Olivine","Zircon","Titanite","Staurolite"]
+phosphates      = ["Apatite","Monazite"]
 
 all_important_minerals=["Pyrope","Almandine","Spessartine","Grossular","Kyanite",
                         "Sillimanite","Andalusite","Gypsum","Baryte","Anhydryte",
@@ -36,17 +36,42 @@ all_important_minerals=["Pyrope","Almandine","Spessartine","Grossular","Kyanite"
                         "Sodalite","Nosean","Hayun","Enstatite","Ferrosilite",
                         "Diopside","Hedenbergite","Jadeite","Augit","Omphazite",
                         "Kaolinite","Illit","Montmorrilonite","Vermiculite",
-                        "Phlogopite","Annit","Eastonite","Muscovite","Phengit",
+                        "Phlogopite","Annite","Eastonite","Muscovite","Phengite",
                         "Paragonite","Quartz","Rutile","Hematite","Illmenite",
-                        "Chromite","Magnetite","Tremolit","Aktinolit","Glaukophan",
-                        "Riebeckit","Hornblende","Anthophyllit","Gredit","Lizadrdit",
+                        "Chromite","Magnetite","Tremolite","Aktinolit","Glaucophane",
+                        "Riebeckite","Hornblende","Anthophyllit","Gredit","Lizadrdit",
                         "Chrysotile","Antigorite","Talk","Chlorite","Clinochlor",
                         "Chamosite","Tourmaline","Lawsonite","Epidote","Zoisite",
                         "Olivine","Zircon","Titanite","Staurolite","Apatite","Monazite"]
-important_minerals = st.selectbox(label="Which group of minerals you want to look at?",options=[garnets,oxides])
 
-if st.button(label='Start'):
-    key = st.secrets["api_key"]  # Adjusted to use square brackets
+mineral_groups = {
+    "Garnets": garnets,
+    "Oxides": oxides,
+    "Micas":micas,
+    "Amphiboles":Amphiboles,
+    "Pyroxenes":Pyroxenes,
+    "Clay Minerals":clay_minerals,
+    "Carbonates":carbonates,
+    "Sulfates":sulfates,
+    "Sulfides":Sulfides,
+    "Phosphates":phosphates,
+    "Neso slicates":nesosilicates,
+    "Ring silicates":ringsilicates,
+    "Group silicates":groupsilicates,
+    "Layered silicates":layeredsilicates,
+    "Alumnosilicates":alumnosilicates,
+    "Feldspars":feldspars,
+    "Foids":foids,
+    "all groups":all_important_minerals
+}
+
+group_name = st.selectbox(label="Which group of minerals do you want to look at?", options=list(mineral_groups.keys()))
+important_minerals = mineral_groups[group_name]
+st.divider()
+
+if st.button(label='Start requesting Information!'):
+    st.write("The selectes Information for your choosen group of minerals are requested from Mindat.org. This process can take some minutes....")
+    key = st.secrets["api_key"]
     MINDAT_API_URL = "https://api.mindat.org"
     headers = {'Authorization': 'Token ' + key}
 
@@ -75,10 +100,17 @@ if st.button(label='Start'):
         if not filtered_results:
             st.warning("No data found for the selected minerals.")
         else:
-            st.markdown("### Details of Selected Important Minerals")
+            st.markdown(f"### Details of Selected Important Minerals from the {group_name} Group")
             for result in filtered_results:
-                name = result.get("name", "N/A")
-                ima_formula = result.get("ima_formula", "N/A")
-                description_short = result.get("description_short", "N/A")
-                mindat_formula = result.get("mindat_formula", "N/A")
-                st.markdown(f"**Name:** {name}\n\n**IMA Formula:** {ima_formula}\n\n**Description:** {description_short}\n\n**MINDAT Formula:** {mindat_formula}")
+                with st.expander(f"**{result.get('name', 'N/A')}**"):
+                    name = result.get("name", "N/A")
+                    ima_formula = result.get("ima_formula", "N/A")
+                    description_short = result.get("description_short", "N/A")
+                    mindat_formula = result.get("mindat_formula", "N/A")
+
+                    col1, col2 = st.columns(2)
+                    with col1:
+                        st.markdown(f"**IMA Formula:** {ima_formula}")
+                        st.markdown(f"**MINDAT Formula:** {mindat_formula}")
+                    with col2:
+                        st.markdown(f"**Description:** {description_short}")
