a197487b4aa769c795ea899e399ae57b5a77d81d
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-05-22 07:57:43 +0000

    commit2_25.05.24

--- Hello.py
+++ Hello.py
@@ -1,74 +1,20 @@
 import streamlit as st
 import requests
 import json
-import os
 
 st.header('An Overview of the Most Important Minerals')
 st.divider()
 st.markdown('This app can be used to get information about the most important minerals in geoscience. The information provided here is requested from mindat.org.')
 st.divider()
+
 selected_fields = st.multiselect(label="Which information do you want to get?", options=['name', 'mindat_formula', 'ima_formula', 'description_short'])
 fields_str = ",".join(selected_fields)
 
-# Lists of important minerals
-garnets = ["Pyrope", "Almandine", "Spessartine", "Grossular"]
-oxides = ["Quartz", "Rutile", "Hematite", "Ilmenite", "Chromite", "Magnetite"]
-alumnosilicates = ["Kyanite", "Sillimanite", "Andalusite"]
-sulfates = ["Gypsum", "Baryte", "Anhydryte"]
-Sulfides = ["Pyrite", "Chalcopyrite"]
-carbonates = ["Calcite", "Aragonite", "Dolomite", "Ankerite", "Siderite", "Magnesite"]
-feldspars = ["Orthoclase", "Albite", "Sanidine", "Microcline", "Anorthite"]
-foids = ["Nepheline", "Leucite", "Sodalite", "Nosean", "Haüyne"]
-Pyroxenes = ["Enstatite", "Ferrosilite", "Diopside", "Hedenbergite", "Augite", "Jadeite", "Omphacite"]
-clay_minerals = ["Kaolinite", "Illite", "Montmorillonite", "Vermiculite"]
-micas = ["Phlogopite", "Annite", "Eastonite", "Muscovite", "Phengite", "Paragonite"]
-Amphiboles = ["Tremolite", "Actinolite", "Glaucophane", "Riebeckite"]
-layeredsilicates = ["Lizadrdite", "Chrysotile", "Antigorite", "Talc", "Chlorite", "Clinochlor", "Chamosite"]
-ringsilicates = ["Tourmaline"]
-groupsilicates = ["Lawsonite", "Epidote", "Zoisite"]
-nesosilicates = ["Olivine", "Zircon", "Titanite", "Staurolite"]
-phosphates = ["Apatite", "Monazite"]
+if 'json_data' not in st.session_state:
+    st.session_state.json_data = None
 
-all_important_minerals = ["Pyrope", "Almandine", "Spessartine", "Grossular", "Kyanite",
-                          "Sillimanite", "Andalusite", "Gypsum", "Baryte", "Anhydryte",
-                          "Pyrite", "Chalcopyrite", "Calcite", "Aragonite", "Dolomite",
-                          "Ankerite", "Siderite", "Magnesite", "Orthoclase", "Albite",
-                          "Sanidine", "Microcline", "Anorthite", "Nepheline", "Leucite",
-                          "Sodalite", "Nosean", "Haüyne", "Enstatite", "Ferrosilite",
-                          "Diopside", "Hedenbergite", "Jadeite", "Omphacite",
-                          "Kaolinite", "Illite", "Montmorillonite", "Vermiculite",
-                          "Phlogopite", "Annite", "Eastonite", "Muscovite", "Phengite",
-                          "Paragonite", "Quartz", "Rutile", "Hematite", "Ilmenite",
-                          "Chromite", "Magnetite", "Tremolite", "Actinolite", "Glaucophane",
-                          "Riebeckite", "Lizadrdite", "Augite",
-                          "Chrysotile", "Antigorite", "Talc", "Chlorite", "Clinochlor",
-                          "Chamosite", "Tourmaline", "Lawsonite", "Epidote", "Zoisite",
-                          "Olivine", "Zircon", "Titanite", "Staurolite", "Apatite", "Monazite"]
-
-mineral_groups = {
-    "Garnets": garnets,
-    "Oxides": oxides,
-    "Micas": micas,
-    "Amphiboles": Amphiboles,
-    "Pyroxenes": Pyroxenes,
-    "Clay Minerals": clay_minerals,
-    "Carbonates": carbonates,
-    "Sulfates": sulfates,
-    "Sulfides": Sulfides,
-    "Phosphates": phosphates,
-    "Nesoslicates": nesosilicates,
-    "Ring silicates": ringsilicates,
-    "Group silicates": groupsilicates,
-    "Layered silicates": layeredsilicates,
-    "Alumnosilicates": alumnosilicates,
-    "Feldspars": feldspars,
-    "Foids": foids,
-    "all groups": all_important_minerals
-}
-
-data_file = "minerals_data.json"
-
-def fetch_and_save_data():
+if st.button(label='Start requesting Information!', use_container_width=True):
+    st.write("The selected information for your chosen group of minerals is requested from Mindat.org. This process can take a few minutes....")
     key = st.secrets["api_key"]
     MINDAT_API_URL = "https://api.mindat.org"
     headers = {'Authorization': 'Token ' + key}
@@ -77,14 +23,13 @@ def fetch_and_save_data():
         'fields': fields_str,
         'format': 'json'
     }
-
+    
     response = requests.get(MINDAT_API_URL + "/minerals_ima/", params=params, headers=headers)
     if response.status_code != 200:
         st.error("Failed to fetch data from the API.")
-        return None
     else:
         json_data = {"results": response.json().get("results", [])}
-
+        
         while response.json().get("next"):
             next_url = response.json()["next"]
             response = requests.get(next_url, headers=headers)
@@ -93,25 +38,55 @@ def fetch_and_save_data():
             else:
                 break
         
-        with open(data_file, "w") as f:
-            json.dump(json_data, f)
-        return json_data
+        st.session_state.json_data = json_data
+        with open('minerals_data.json', 'w') as json_file:
+            json.dump(json_data, json_file)
 
-json_data = None
+if st.session_state.json_data:
+    mineral_groups = {
+        "Garnets": ["Pyrope", "Almandine", "Spessartine", "Grossular"],
+        "Oxides": ["Quartz", "Rutile", "Hematite", "Ilmenite", "Chromite", "Magnetite"],
+        "Micas": ["Phlogopite", "Annite", "Eastonite", "Muscovite", "Phengite", "Paragonite"],
+        "Amphiboles": ["Tremolite", "Actinolite", "Glaucophane", "Riebeckite"],
+        "Pyroxenes": ["Enstatite", "Ferrosilite", "Diopside", "Hedenbergite", "Jadeite", "Omphacite"],
+        "Clay Minerals": ["Kaolinite", "Illite", "Montmorillonite", "Vermiculite"],
+        "Carbonates": ["Calcite", "Aragonite", "Dolomite", "Ankerite", "Siderite", "Magnesite"],
+        "Sulfates": ["Gypsum", "Baryte", "Anhydryte"],
+        "Sulfides": ["Pyrite", "Chalcopyrite"],
+        "Phosphates": ["Apatite", "Monazite"],
+        "Nesoslicates": ["Olivine", "Zircon", "Titanite", "Staurolite"],
+        "Ring silicates": ["Tourmaline"],
+        "Group silicates": ["Lawsonite", "Epidote", "Zoisite"],
+        "Layered silicates": ["Lizadrdite", "Chrysotile", "Antigorite", "Talc", "Chlorite", "Clinochlor", "Chamosite"],
+        "Alumnosilicates": ["Kyanite", "Sillimanite", "Andalusite"],
+        "Feldspars": ["Orthoclase", "Albite", "Sanidine", "Microcline", "Anorthite"],
+        "Foids": ["Nepheline", "Leucite", "Sodalite", "Nosean", "Haüyne"],
+        "all groups": [
+            "Pyrope", "Almandine", "Spessartine", "Grossular", "Kyanite",
+            "Sillimanite", "Andalusite", "Gypsum", "Baryte", "Anhydryte",
+            "Pyrite", "Chalcopyrite", "Calcite", "Aragonite", "Dolomite",
+            "Ankerite", "Siderite", "Magnesite", "Orthoclase", "Albite",
+            "Sanidine", "Microcline", "Anorthite", "Nepheline", "Leucite",
+            "Sodalite", "Nosean", "Haüyne", "Enstatite", "Ferrosilite",
+            "Diopside", "Hedenbergite", "Jadeite", "Omphacite",
+            "Kaolinite", "Illite", "Montmorillonite", "Vermiculite",
+            "Phlogopite", "Annite", "Eastonite", "Muscovite", "Phengite",
+            "Paragonite", "Quartz", "Rutile", "Hematite", "Ilmenite",
+            "Chromite", "Magnetite", "Tremolite", "Actinolite", "Glaucophane",
+            "Riebeckite", "Lizadrdite", "Augite", "Chrysotile", "Antigorite",
+            "Talc", "Chlorite", "Clinochlor", "Chamosite", "Tourmaline",
+            "Lawsonite", "Epidote", "Zoisite", "Olivine", "Zircon", "Titanite", "Staurolite", "Apatite", "Monazite"
+        ]
+    }
 
-if not os.path.exists(data_file):
-    if st.button(label='Start requesting Information!', use_container_width=True):
-        st.write("The selected information for your chosen group of minerals is requested from Mindat.org. This process can take a few minutes....")
-        json_data = fetch_and_save_data()
-else:
-    with open(data_file, "r") as f:
-        json_data = json.load(f)
-
-if json_data:
     group_name = st.selectbox(label="Which group of minerals do you want to look at?", options=list(mineral_groups.keys()))
     important_minerals = mineral_groups[group_name]
     st.divider()
-    
+
+    with open('minerals_data.json', 'r') as json_file:
+        json_data = json.load(json_file)
+
+    # Filter results for important minerals
     filtered_results = [result for result in json_data["results"] if result.get("name") in important_minerals]
     
     if not filtered_results:
@@ -129,6 +104,5 @@ if json_data:
                 with col1:
                     st.markdown(f"**IMA Formula:** {ima_formula}")
                     st.markdown(f"**MINDAT Formula:** {mindat_formula}")
-
                 with col2:
                     st.markdown(f"**Description:** {description_short}")
